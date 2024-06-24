cd56333a0a18e56c77c37adf67ac6b9a36110dc8
MaratXcode <kosjachok8@yandex.ru>
2024-06-24 04:43:16 +0300

    work with storyboard and view controllers

---
+++ AboutMeApp.V5/PicturesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PicturesViewController.swift
+//  AboutMeApp.V5
+//
+//  Created by Marat Fakhrizhanov on 24.06.2024.
+//
+
+import UIKit
+
+class PicturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
